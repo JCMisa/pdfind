c35ff6b55add2010a32e5fe684abbba212992809
JC MISA <johncarlomisa399@gmail.com>
2024-11-21 18:42:55 +0800

    design the dashboard page

---
+++ app/(routes)/dashboard/_components/Sidebar.tsx
@@ -0,0 +1,44 @@
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
+import { LayoutDashboard, Shield } from "lucide-react";
+import Image from "next/image";
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="shadow-md h-screen p-7 bg-dark-100">
+      <div className="flex items-center gap-1">
+        <Image
+          src={"/logo.svg"}
+          alt="logo"
+          width={1000}
+          height={1000}
+          className="w-10 h-10"
+        />
+        <p className="text-xl font-bold">
+          <span className="text-primary">PDF</span>ind
+        </p>
+      </div>
+
+      <div className="mt-10">
+        <Button className="w-full text-dark-100 font-bold">+ Upload PDF</Button>
+
+        <div className="flex gap-2 items-center p-3 mt-5 hover:bg-dark transition-all rounded-lg cursor-pointer">
+          <LayoutDashboard className="w-4 h-4" />
+          <h2>Workspace</h2>
+        </div>
+        <div className="flex gap-2 items-center p-3 mt-1 hover:bg-dark transition-all rounded-lg cursor-pointer">
+          <Shield className="w-4 h-4" />
+          <h2>Upgrade</h2>
+        </div>
+      </div>
+      <div className="absolute bottom-24 w-[80%]">
+        <Progress value={33} />
+        <p className="text-sm mt-1">2 out of 5 PDF Uploaded</p>
+        <p className="text-xs text-gray-400 mt-2">Upgrade to upload more PDF</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- app/(routes)/dashboard/layout.tsx
+++ app/(routes)/dashboard/layout.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Sidebar from "./_components/Sidebar";
+import Header from "./_components/Header";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <div className="md:w-64 h-screen fixed">
+        <Sidebar />
+      </div>
+      <div className="md:ml-64">
+        <Header />
+        <div className="p-7">{children}</div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardLayout;
